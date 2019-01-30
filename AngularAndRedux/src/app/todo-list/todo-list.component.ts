8631b64dc4819899a9810232efe91c98b5fc903f
MolnarArthur <arthur.molnar@usource.ro>
2019-01-30 15:14:03 +0200

    Todo list

--- AngularAndRedux/src/app/todo-list/todo-list.component.ts
+++ AngularAndRedux/src/app/todo-list/todo-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgRedux, select } from '@angular-redux/store';
+import { IAppState } from '../store';
+import { ADD_TODO, REMOVE_TODO, TOGGLE_TODO } from '../actions';
+import { ITodo } from '../todo';
 
 @Component({
   selector: 'app-todo-list',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
-  constructor() { }
+  @select() todos;
+
+  model: ITodo = {
+    id: 0,
+    description: "",
+    responsible: "",
+    priority: "low",
+    isCompleted: false
+  }
+
+  constructor(private ngRedux: NgRedux<IAppState>) { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    this.ngRedux.dispatch({type: ADD_TODO, todo: this.model})
+  }
+
+  toggleTodo(todo) {
+    this.ngRedux.dispatch({type: TOGGLE_TODO, id: todo.id})
+  }
+
+  removeTodo(todo) {
+    this.ngRedux.dispatch({type: REMOVE_TODO, id: todo.id})
   }
 
 }
